1830a186d61400498da9654b2f003b10df37ac6e
Sai Charan <venkatasaicharan.a@gmail.com>
2024-05-19 16:54:22 +0530

    Final project

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Appanavenkatasaicharan"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://x.com/venkatasai74829"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/venkata-sai-charan-appana-083376229/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/a.v.s.charan/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
---
+++ src/components/Projects/projectData.js
@@ -0,0 +1,51 @@
+import chat from "../../Assets/Projects/chatapp.png";
+import delivery from "../../Assets/Projects/delivery.png";
+import journal from "../../Assets/Projects/journal.png";
+import notes from "../../Assets/Projects/notesapp.png";
+import quiz from "../../Assets/Projects/quizlet.png";
+
+const data = [
+    {
+        imgPath:chat,
+        isBlog:false,
+        title:"SafeComm",
+        description:"",
+        ghLink:"https://github.com/Appanavenkatasaicharan/chat-app-with-end-to-end-encryption",
+        demoLink:"https://chat-app-with-end-to-end-encryption-frontend.vercel.app/"
+    },
+    {
+        imgPath:delivery,
+        isBlog:false,
+        title:"Tomato",
+        description:"",
+        ghLink:"https://github.com/Appanavenkatasaicharan/busybeecafe-frontend",
+        demoLink:"https://busybeecafe-frontend.vercel.app/"
+    },
+    {
+        imgPath:journal,
+        isBlog:false,
+        title:"JournalPlus",
+        description:"",
+        ghLink:"https://github.com/Appanavenkatasaicharan/JournalPlus",
+        demoLink:"https://journal-plus-frontend.vercel.app/"
+    },
+    {
+        imgPath:notes,
+        isBlog:false,
+        title:"NoteDown",
+        description:"",
+        ghLink:"https://github.com/Appanavenkatasaicharan/notes-app",
+        demoLink:"https://vermillion-kataifi-c687f4.netlify.app/"
+    },
+    {
+        imgPath:quiz,
+        isBlog:false,
+        title:"Quizlet",
+        description:"",
+        ghLink:"https://github.com/Appanavenkatasaicharan/quizzical",
+        demoLink:"https://lighthearted-otter-f5d651.netlify.app/"
+    }
+]
+
+export default data;
+    
